46b964581f15b6f3b19754bf68db6fd71fd19a33
Omer Solmazer <os788@nyu.edu>
2017-05-04 12:57:23 -0400

    Server connection bugs fixed. Options added to entries. HTML updated to look more beautiful

--- sam.js
+++ sam.js
@@ -4,102 +4,126 @@
 
 var lower_case = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 var upper_case = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ]
-var punctuation= ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '[', ']', '?', '_', '-', '@', '{', '}', '^', '=', ';' ]
+var punctuation= ['!', '#', '$', '%', '&', '*', '+', ',', '-', '.', '/', '?', '_', '-', '@', '^', '=', ';' ]
 var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 var background_page = chrome.extension.getBackgroundPage();
-
-// Sample vault data: 
-
-// var sample_vault_data = [
-// { "username" : "-Nnz5$]0", "password" : "nr70jR38a6@b!8%/", "domain" : 'facebook' },
-// { "username" : "[D!t4Eae", "password" : "l8i2G20dgx2f0/Gb", "domain" : 'google' },
-// { "username" : "oo1Q4)U6", "password" : "60/cHQI2+65-OY6(", "domain" : 'twitter' }
-// ];
-
+var back_cons = background_page.console;
+var url = '';
 
 // Fired upon user clicking on login button, if they are not already logged in.
 function login() {
-  // background_page.save_vault(sample_vault_data, function(){});
   var username = document.getElementById('sam_username').value;
   var pw = document.getElementById('sam_password').value;
-  background_page.login(username, pw);
-  background_page.get_vault(function(data){
-    populate_table('sam_pws_table', data);
+  background_page.login(username, pw, function(data){
+    if(data) {
+      document.getElementById('sam_err_msg').hidden = true;
+      background_page.get_remote_vault(function(data){
+        populate_table('sam_pws_table', data);
+      });
+    }
+    else{
+      document.getElementById('sam_err_msg').hidden = false;
+    }
   });
 }
 
 // Generate new username and password pair.
-// Will be updated to include options for different groups and lengths
-// TODO 
 function generate_creds() {
   document.getElementById('sam_new_creds').hidden = false;
   var username_box = document.getElementById('sam_new_username');
   var pw_box = document.getElementById('sam_new_password');
-  var name = gen_rand_seq(8, true, true);
-  var pw = gen_rand_seq(16, true, true);
+
+  var un_cap = document.getElementById('sam_check_name_cap').checked;
+  var un_dig = document.getElementById('sam_check_name_dig').checked;
+  var un_sym = document.getElementById('sam_check_name_sym').checked;
+  var un_len = document.getElementById('sam_username_slider').value;
+
+  var pw_cap = document.getElementById('sam_check_password_cap').checked;
+  var pw_dig = document.getElementById('sam_check_password_dig').checked;
+  var pw_sym = document.getElementById('sam_check_password_sym').checked;
+  var pw_len = document.getElementById('sam_password_slider').value;
+
+  var name = gen_rand_seq(un_len, un_cap, un_sym, un_dig);
+  var pw = gen_rand_seq(pw_len, pw_cap, pw_sym, pw_dig);
   username_box.innerHTML = name;
   pw_box.innerHTML = pw;
 }
 
 // Generating random sequence from the character groups
-// Will be updated to use inclusion flags
-// TODO
-function gen_rand_seq(limit, include_punc, include_dig) {
+function gen_rand_seq(limit, include_cap, include_sym, include_dig) {
   var r = "";
   // Populate the string
-  for (var i = 0; i < limit; i++) {
+  while(r.length < limit){
     switch(Math.floor(Math.random()*4)){
       case 0:
-      r += upper_case[Math.floor(Math.random()*upper_case.length)];
+      if(include_cap){
+        r += upper_case[Math.floor(Math.random()*upper_case.length)];
+      }
       break;
+      
       case 1:
-      r += punctuation[Math.floor(Math.random()*punctuation.length)];
+      if(include_sym){
+        r += punctuation[Math.floor(Math.random()*punctuation.length)];
+      }
       break;
+      
       case 2:
-      r += digits[Math.floor(Math.random()*digits.length)];
-      break;
-      default:
-      r += lower_case[Math.floor(Math.random()*lower_case.length)];
+      if(include_dig){
+        r += digits[Math.floor(Math.random()*digits.length)];
+      }
       break;
 
+      default:
+        r += lower_case[Math.floor(Math.random()*lower_case.length)];
+      break;
     }
   }
+
+
   // Make sure it has at least 1 of each category
   var low = false, up = false, punc = false, dig = false;
+  
   for (var i = 0; i < lower_case.length; i++) {
     if(r.includes(lower_case[i])){
       low = true;
       break;
     }
   }
-  for (var i = 0; i < upper_case.length; i++) {
-    if(r.includes(upper_case[i])){
-      up = true;
-      break;
+
+  if(include_cap){
+    for (var i = 0; i < upper_case.length; i++) {
+      if(r.includes(upper_case[i])){
+        up = true;
+        break;
+      }
     }
   }
-  for (var i = 0; i < punctuation.length; i++) {
-    if(r.includes(punctuation[i])){
-      punc = true;
-      break;
+
+  if(include_sym){
+    for (var i = 0; i < punctuation.length; i++) {
+      if(r.includes(punctuation[i])){
+        punc = true;
+        break;
+      }
     }
   }
-  for (var i = 0; i < digits.length; i++) {
-    if(r.includes(digits[i])){
-      dig = true;
-      break;
+
+  if(include_dig){
+    for (var i = 0; i < digits.length; i++) {
+      if(r.includes(digits[i])){
+        dig = true;
+        break;
+      }
     }
   }
   // Only pass if all exist
-  var is_valid = low && up && punc && dig;
+  var is_valid = low && (up || !include_cap) && (punc || !include_sym) && (dig || !include_dig);
   if(!is_valid){
-    r = gen_rand_seq(limit, include_punc, include_dig);
+    r = gen_rand_seq(limit, include_cap, include_sym, include_dig);
   }
   return r;
 }
 
-// Print out usernames and passwords as a table. Add edit button. Instead of edit how about add custom pw?
-// TODO
 function populate_table(table_name, data) {
   document.getElementById('sam_login_div').hidden = true;
   document.getElementById('sam_pws_div').hidden = false;
@@ -107,9 +131,13 @@ function populate_table(table_name, data) {
 
   var t = new Array(), j = -1;
   t[++j] = '<tr><th>Domain</th><th>User Name</th><th>Password</th><th style="color:red">Remove</th></tr>';
-
   for (var i = 0; i < data.length; i++) {
-    t[++j] ='<tr><td>';
+    if(data[i]['domain'] == url || url.includes(data[i]['domain'])){
+      t[++j] = '<tr bgcolor=#00FF00><td>'; 
+    }
+    else{
+      t[++j] ='<tr><td>';
+    }
     t[++j] = data[i]['domain'];
     t[++j] = '</td><td>';
     t[++j] = data[i]['username'];
@@ -158,14 +186,10 @@ function copy_username() {
   copy_to_clipboard(document.getElementById('sam_new_username').innerHTML);
   var un = document.getElementById('sam_new_username').innerHTML;
   var pw = document.getElementById('sam_new_password').innerHTML;
-  chrome.tabs.getSelected(null, function (tab) {
-    var url = new URL(tab.url);
-    var domain = url.hostname;
-    var data = { 'domain' : domain, 'username' : un, 'password' : pw };
-    background_page.add_to_vault(data, function(data){
+  var data = { "domain" : url, "username" : un, "password" : pw };
+    background_page.add_to_remote_vault(data, function(data){
       populate_table('sam_pws_table', data);
     });
-  });
 }
 
 // Save the generated username and password (assumption: if copied, it is used) while copying the password to clipboard
@@ -173,22 +197,36 @@ function copy_password() {
   copy_to_clipboard(document.getElementById('sam_new_password').innerHTML);
   var un = document.getElementById('sam_new_username').innerHTML;
   var pw = document.getElementById('sam_new_password').innerHTML;
-  chrome.tabs.getSelected(null, function (tab) {
-    var url = new URL(tab.url);
-    var domain = url.hostname;
-    var data = { 'domain' : domain, 'username' : un, 'password' : pw };
-    background_page.add_to_vault(data, function(data){
+  var data = { "domain" : url, "username" : un, "password" : pw };
+    background_page.add_to_remote_vault(data, function(data){
       populate_table('sam_pws_table', data);
     });
-  });
+}
+
+function update_username_length() {
+  document.getElementById('sam_username_length').innerHTML=this.value;
+}
+
+function update_password_length() {
+  document.getElementById('sam_password_length').innerHTML=this.value;
+}
+
+function logout() {
+  background_page.logout();
+  location.reload(true);
 }
 
 // Add event listeners and button onclick functions to the HTML elements.
 document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
+    chrome.tabs.getSelected(null, function (tab) {
+      var temp = new URL(tab.url);
+      temp = temp.hostname.replace(/www\./g, '');
+      url = temp;
+    });
     background_page.check_login(function(cookie_value){
       if(cookie_value) {
-        background_page.get_vault(function(data) {
+        background_page.get_remote_vault(function(data) {
           populate_table('sam_pws_table', data );
         });
       }
@@ -196,11 +234,13 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('not logged in');
       }
     });
-    
     document.getElementById('sam_login').onclick = login;
     document.getElementById('sam_gen_data').onclick = generate_creds;
     document.getElementById('sam_username_copy').onclick = copy_username;
     document.getElementById('sam_password_copy').onclick = copy_password;
+    document.getElementById('sam_logoff').onclick = logout;
+    document.getElementById('sam_username_slider').onchange = update_username_length;
+    document.getElementById('sam_password_slider').onchange = update_password_length;
   });
 });
 
--- sam_background.js
+++ sam_background.js
@@ -6,12 +6,7 @@
 // 	}
 // );
 
-// For clearing out the local storage
-function clear_storage() {
-    chrome.storage.local.clear(function() {});
-} 
-
-var sam_cookie_url = "http://www.example.com/";
+var sam_cookie_url = "https://pcs-sam.mybluemix.net/";
 var url_base_auth = 'https://pcs-sam-auth.mybluemix.net/';
 var url_base_vault = 'https://pcs-sam-vault.mybluemix.net/';
 var token = ' ';
@@ -20,18 +15,27 @@ var uid = -1;
 // Check if the user logged in. Currently only checks if the cookie exists, later will send request to server for making sure the token is still valid.
 // TODO
 // Post for getting auth: auth/token/
-// Get user vault: /vault/users/<id>
+// Get user vault: /vault/<id>
 // 
 
-function send_login_request(type, url, data, success_callback, error_callback) {
+// Helper post function
+function send_request(type, url, d, success_callback, error_callback) {
   $.ajax({
     type : type,
     url : url,
     dataType: "text",
     contentType: "application/json; charset=utf-8",
-    data : JSON.stringify(data),
+    headers: {
+      "Authorization": "bearer "+token
+    },
+    data : JSON.stringify(d),
 
     success: function(msg){
+        try{
+            msg = JSON.parse(msg);
+        }
+        catch(SyntaxError){
+        }
         success_callback(msg);
     },
 
@@ -41,48 +45,34 @@ function send_login_request(type, url, data, success_callback, error_callback) {
   }); 
 }
 
-// Helper post function
-function send_request(type, url, data, success_callback, error_callback) {
-  $.ajax({
-    type : type,
-    url : url,
-    dataType: "text",
-    contentType: "application/json; charset=utf-8",
-    headers: {
-      "Authorization": token
-    },
-    data : JSON.stringify(data),
-
-    success: function(msg){
-      success_callback(msg);
-    },
-
-    error: function (textStatus, errorThrown) {
-      error_callback(JSON.stringify(textStatus), errorThrown);
-    }
-  }); 
-}
-
-function login(username, password) {
+function login(username, password, call_back) {
     send_request("POST", url_base_auth+"auth/token", { "email": username, "password": password}, 
     function(data){
         token = data;
         uid = get_id_from_token(token);
         set_login(token);
+        // console.log(token);
+        call_back(data);
     },
     function(status, error){
+        call_back(null);
         console.log('Error logging in: ' + status + ' ' + error);
     });
 }
 
+function logout() {
+    chrome.cookies.remove({"url": sam_cookie_url, "name": "sam"}, function(){});
+    token = ' ';
+    uid = -1;
+}
+
 function check_login(call_back) {
     chrome.cookies.get({"url": sam_cookie_url, "name": "sam"}, function(cookie) {
-        if (call_back && cookie) {
+        if (cookie) {
             call_back(cookie.value);
-            console.log(cookie.value);
         }
         else {
-            call_back();
+            call_back(null);
         }
     } );
 }
@@ -94,23 +84,41 @@ function set_login(token) {
     })
 }
 
-// Save the vault to the chrome storage.
-function save_vault(data, call_back) {
-    // Make sure data exists.
-    if (!data) {
-        message('Error: No value specified');
-        return;
+function save_remote_vault(data, call_back) {
+    if(data) {
+        send_request("PUT", url_base_vault+"vault", { "user_id": uid, "data": data},
+            function(response) {
+                call_back(response["data"]);
+            },
+            function(status, error) {
+                // console.log('Saving did not work. Error: ' + JSON.stringify(status));
+            });
     }
-    // Save it using the Chrome extension storage API.
-    data = encrypt(data);
-    chrome.storage.local.set({'sam_vault': data}, call_back() );
 }
 
-// Get the vault from local storage.
-function get_vault(call_back) {
-    chrome.storage.local.get('sam_vault', function (data) {
-        call_back(JSON.parse(decrypt(data['sam_vault'])));
-    }) 
+function get_remote_vault(call_back) {
+    if(uid == -1)
+        return;
+    send_request("GET", url_base_vault+"vault/"+uid, '', 
+    function(data){
+        var list = data["data"];
+        if(list != null) {
+            list = JSON.parse(list);
+            call_back(list);
+        }
+    },
+    function(status, error){
+        var err = status;
+        if(err['status'] == 404) {
+            send_request("POST", url_base_vault+"vault", { "user_id": uid }, 
+            function(data){
+                call_back(data["data"]);
+            },
+            function(status, error) {
+                console.log('Error from Post ? ' + JSON.stringify(status));
+            });
+        }
+    });
 }
 
 // Pass phrase. Will be altered later.
@@ -127,13 +135,13 @@ function decrypt(data) {
         return CryptoJS.AES.decrypt(data, x).toString(CryptoJS.enc.Utf8);
 }
 
-function add_to_vault(data, call_back) {
-    get_vault(function(curr_vault) {
+function add_to_remote_vault(data, call_back) {
+    get_remote_vault(function(curr_vault) {
         var found = false;
         // Update a domain
         for(var i = 0; i < curr_vault.length; i++) {
-            if(curr_vault[i]['domain'] == data['domain']){
-                if (confirm("This will overwrite the username \""+curr_vault[i]['username']+"\" and password for "+data['domain']+". Do you wish to continue?") == true) {
+            if(curr_vault[i]["domain"] == data["domain"]){
+                if (confirm("This will overwrite the username \""+curr_vault[i]["username"]+"\" and password for "+data["domain"]+". Do you wish to continue?") == true) {
                     curr_vault[i] = data;
                 }
                 found = true;
@@ -141,26 +149,31 @@ function add_to_vault(data, call_back) {
         }
         //Append
         if(!found){
+            try{
+                curr_vault = JSON.parse(curr_vault);
+            } catch (SyntaxError) {
+
+            }
             curr_vault.push(data);
         }
-        save_vault(curr_vault, function(){
-            call_back(curr_vault);
+        save_remote_vault(curr_vault, function(new_data){
+            call_back(new_data);
         });
     });
 }
 
 function get_id_from_token(token) {
-    return JSON.parse(b64utos(token.split('.')[1]))['user_id'];
+    return JSON.parse(b64utos(token.split('.')[1]))["user_id"];
 }
 
 function remove_from_vault(domain, call_back) {
     if (confirm("Are you sure you wish to delete the entry for "+domain+"?") == true) {
-        get_vault(function(curr_vault) {
+        get_remote_vault(function(curr_vault) {
             for(var i = 0; i < curr_vault.length; i++) {
-                if(curr_vault[i]['domain'] == domain) {
+                if(curr_vault[i]["domain"] == domain) {
                     curr_vault.splice(i, 1);
-                    save_vault(curr_vault, function(){
-
+                    save_remote_vault(curr_vault, function(){
+                        console.log(domain + ' removed from vault');
                     });
                     call_back(curr_vault);
                     return;
